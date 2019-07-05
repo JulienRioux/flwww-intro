46f00e583b7a13d00602164b603364c2749d5a07
Julien Rioux <julien.rioux.webdev@gmail.com>
2019-07-05 19:04:43 -0400

    Adding the theme provider

--- src/App.js
+++ src/App.js
@@ -1,26 +1,138 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Import the flwww Button and message
+import { Modal, Button, Input, message } from "flwww";
+
+class App extends Component {
+	state = {
+	    modalIsVisible: false,  // This control if the modal is visible or not
+			username: "",
+			password: "",
+			isLoading: false,
+		}
+
+	  toggleModal = () => {
+	    // This function change the state of the modal visibility (e.g. this.state.modalIsVisible)
+	    this.setState(prevState => ({ modalIsVisible: !prevState.modalIsVisible }));
+	  }
+
+		handleChange = (e) => {
+			this.setState({
+				[e.target.name]: e.target.value,
+			});
+		}
+
+		// handle signup submit signin form
+		handleSubmit = () => {
+			const { password, username } = this.state;
+			// Form validation
+			if(username.length < 1){
+				message("You need to add a usernamme!", "error");
+			}
+			else if(password.length < 1){
+				message("You need to add a password!", "error");
+			}
+			else {
+				// Make a fake API call
+				this.setState({ isLoading: true });
+				setTimeout(() => {
+					// End the loading and close the modal
+					this.setState({
+						isLoading: false,
+						modalIsVisible: false,
+					});
+					// Send a success message when the user have log in
+					message(`Welcome back ${ username }!`, "success");
+				}, 1000);
+			}
+		}
+
+	  render(){
+	    const { modalIsVisible, username, password } = this.state;
+
+	    return (
+	      <div>
+	        <Button
+						onClick={ this.toggleModal }
+						style={{ margin: "2rem" }}>Sign in</Button>
+
+	        <Modal
+	          isVisible={ modalIsVisible }
+	          toggleModal={ this.toggleModal }>
+								<SigninModal
+									toggleModal={ this.toggleModal } />
+	        </Modal>
+	      </div>
+	    )
+	  }
 }
 
 export default App;
+
+
+class SigninModal extends Component {
+	state = {
+			username: "",
+			password: "",
+			isLoading: false,
+		}
+
+		handleChange = (e) => {
+			this.setState({
+				[e.target.name]: e.target.value,
+			});
+		}
+
+		// handle signup submit signin form
+		handleSubmit = () => {
+			const { password, username } = this.state;
+			// Form validation
+			if(username.length < 1){
+				message("You need to add a usernamme!", "error");
+			}
+			else if(password.length < 1){
+				message("You need to add a password!", "error");
+			}
+			else {
+				// Make a fake API call
+				this.setState({ isLoading: true });
+				setTimeout(() => {
+					// Close the modal
+					this.props.toggleModal();
+					// Send a success message when the user have log in
+					message(`Welcome back ${ username }!`, "success");
+				}, 1000);
+			}
+		}
+
+	  render(){
+	    const { username, password } = this.state;
+
+	    return (
+	      <div>
+					<h2 style={{ marginTop: "0" }}>Sign in</h2>
+
+					<label>Username:</label>
+					<Input
+						value={ username }
+						name="username"
+						onChange={ this.handleChange }
+						style={{ margin: ".7rem 0" }}
+						type="text"
+						placeholder="Enter your username here" />
+					<label>Password:</label>
+					<Input
+						value={ password }
+						name="password"
+						onChange={ this.handleChange }
+						style={{ margin: ".7rem 0 1.5rem" }}
+						type="password"
+						placeholder="Enter your password" />
+					<Button
+						loading={ this.state.isLoading }
+						onClick={ this.handleSubmit }
+						type="primary">Submit</Button>
+	      </div>
+	    )
+	  }
+}
--- src/index.js
+++ src/index.js
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// Import the ThemeProvider
+import { ThemeProvider } from "flwww";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Changing the colors
+const theme = {
+	defaultColor: "#7a00ff",
+};
+
+
+ReactDOM.render(
+	<ThemeProvider theme={ theme }>
+		<App />
+	</ThemeProvider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
