8388c8e9151085f5d6d938fd390f5a742880649c
Julien Rioux <julien.rioux.webdev@gmail.com>
2019-07-10 22:43:39 -0400

    ready to add the theme customization

--- src/index.js
+++ src/index.js
@@ -3,20 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// Import the ThemeProvider
-import { ThemeProvider } from "flwww";
-
-// Changing the colors
-const theme = {
-	primaryColor: "#29bb66",
-};
 
 
-ReactDOM.render(
-	<ThemeProvider theme={ theme }>
-		<App />
-	</ThemeProvider>
-, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
 serviceWorker.unregister();
